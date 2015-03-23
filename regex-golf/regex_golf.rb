3a99047658418ddc0a2a6ad32a7470f7472ee33e
Sam Shiffman <sam@samshiffman.com>
2015-03-22 21:34:09 -0700

    create working basic regex tests

--- regex-golf/regex_golf.rb
+++ regex-golf/regex_golf.rb
@@ -1,7 +1,48 @@
-#!usr/bin/env ruby
+#!/usr/bin/env ruby
 
 ## Ruby application to test regular expression before entering into Regex Golf
 ## See <regex.alf.nu>
 
+words_to_match = String.new
+words_to_reject = String.new
+#to_match = words_to_match.split
+#to_reject = words_to_reject.split
+#all_words = to_match + to_reject
 
-puts "Enter each word"
+
+class StringMatcher
+
+	# attr_accessor :all_words, :regex
+
+	def initialize(to_match, to_reject, regex_string = String.new)
+		@to_match = to_match
+		@to_reject = to_reject
+		@all_words = to_match + to_reject
+		# @regex_string = regex_string
+		@regex = Regexp.new(regex_string)
+	end
+
+	def match_words?
+		@all_words.map do |word|
+			word.match(@regex).string unless word.match(@regex).nil?
+		end.compact == @to_match
+	end
+
+end
+
+class RegexPrinter
+
+	def initialize(regex_string)
+		@regex = Regexp.new(regex_string)
+	end
+
+	def regex_printer
+		"regex printed"
+	end
+
+	def remove_regex_delimiters # Regex Golf site requires delimiter removal
+		@regex.inspect[1...-1]
+	end
+
+end
+
--- regex-golf/test_regex_golf.rb
+++ regex-golf/test_regex_golf.rb
@@ -9,17 +9,50 @@ Minitest::Reporters.use!
 
 class TestRegexGolf < Minitest::Test
 
-	def test_assert_equal_from_test_file
-		x = 2
-		assert_equal 2, x
-	end 
+	def setup
+		@to_match = ["abac", "accede", "adead", "babe", "bead",
+								"bebed", "bedad", "bedded", "bedead", "bedeaf",
+								"caba", "caffa", "dace", "dade", "daff", "dead", 
+								"deed", "deface", "faded", "faff", "feed"]
+		@to_reject = ["beam", "buoy", "canjac", "chymia", "corah", 
+								"cupula", "griece", "hafter", "idic", "lucy",
+								"martyr", "matron", "messrs", "mucose", "relose", 
+								"sonly", "tegua", "threap", "towned", "widish", "yite"]
+		
+		@all_words = 						@to_reject + @to_match
+	
+		
+		#@to_match_with_typo = 	(@ to_match[0..-2] << "veed").join(' ')
+
+		@regex_string = "[a-df][a-e][a-f]{2}\w?"
+		@r = RegexPrinter.new(@regex_string)
+		@m = 						StringMatcher.new(@to_match, @to_reject, @regex_string)
+		#@m_with_typo = StringMatcher.new(@to_match_with_typo, @regex_string)
+	end
+
+	def test_regex_printer
+		assert_equal "regex printed", @r.regex_printer
+	end
+
+	def test_match_words_method_passes_when_correct
+		assert @m.match_words?
+	end
+
+	# def test_match_words_fails_when_match_word_list_has_typo
+	# 	refute @m_with_typo.match_words?
+	# end
+
+
+	def test_final_result_is_string_no_delimiters
+		assert_equal String, @r.remove_regex_delimiters.class
+		assert_equal @regex_string, @r.remove_regex_delimiters
+	end
+
 
 end
 
-=begin
 
-	def setup
-	end
+=begin
 
 	def test_words_to_match_populate_array
 		assert_equal to_match.class, Array
@@ -60,12 +93,5 @@ end
 
 	def test_regex_matches_all_accept_words
 	end
-
-	def final_regex_is_string
-	end
-
-	def final_regex_has_no_forward_slashes
-	end
-
 
 =end
