cee0af177b8e550197f3b7a0208d3a2abefc334c
Sam Shiffman <sam@samshiffman.com>
2015-03-24 12:52:27 -0700

    create main app classes

--- regex-golf/regex_golf.rb
+++ regex-golf/regex_golf.rb
@@ -3,46 +3,118 @@
 ## Ruby application to test regular expression before entering into Regex Golf
 ## See <regex.alf.nu>
 
-words_to_match = String.new
-words_to_reject = String.new
+## copy to /usr/local/bin so it's in $PATH?
+
+#words_to_match = String.new
+#words_to_reject = String.new
 #to_match = words_to_match.split
 #to_reject = words_to_reject.split
 #all_words = to_match + to_reject
 
+GETWORDS = <<-TEXT 
+Enter your list of words to match.
+Press ENTER after each word.
+Press ENTER twice in a row when you are finished.
+TEXT
+
+CHECKLIST = <<-TEXT
+Check this list. Are they any misspelled words?
+  Don't worry yet if the list is missing a word --
+    We'll get to that later.
+Are there any misspelled words? [Y/n] 
+TEXT
+
+print GETWORDS, "\n"
+print CHECKLIST, "\n"
+
+=begin
+
+class WordGetter
+
+  def get_match_words
+    print GETWORDS,'\n'
+    to_match = gets("\n\n").chomp
+    print_word_list(to_match, "match")
+    check_list_complete(to_match)
+  end
+
+  def check_list_for_errors(words = to_match)
+    print CHECKLIST
+    complete = gets.chomp.downcase
+    while complete != "n"
+      puts "Type the misspelled words here:"
+      misspelled = gets("\n\n").chomp
+      misspelled.each do |m|
+        words = words.split.delete(m).join('\n') # May not update list if words doesn't point to correct list
+      end
+      check_list_for_errors(words) # May not update list if words doesn't point to correct list
+    end
+  
+  end
+
+
+  def get_reject_words
+    print GETWORDS,'\n'
+    to_reject = gets("\n\n").chomp
+    print_word_list(to_reject, "reject")
+    check_list_complete(to_reject)
+
+  end
+
+  def print_word_list(words = to_match, match_reject = "match")
+    puts "This is your list of words to #{match_reject}. Please check it:"
+    words.split.sort.each {|w| puts w}
+  end
+
+  def check_list_complete(words = to_match)
+    puts "Is the list complete? (Y for Yes, any other key for No)"
+    complete = gets.chomp.downcase
+    while complete != "y"
+      puts "Add the missing words here:"
+      to_match += gets("\n\n").chomp
+      print_word_list(to_match, "match")
+      puts "Is the list complete? (Y for Yes, N for No)"
+      complete = gets.chomp.downcase
+    end
+  end
+
+end
+
+=end
 
 class StringMatcher
 
-	# attr_accessor :all_words, :regex
+  # attr_accessor :all_words, :regex
 
-	def initialize(to_match, to_reject, regex_string = String.new)
-		@to_match = to_match
-		@to_reject = to_reject
-		@all_words = to_match + to_reject
-		# @regex_string = regex_string
-		@regex = Regexp.new(regex_string)
-	end
+  def initialize(to_match, to_reject, regex_string = String.new)
+    @to_match = to_match
+    @to_reject = to_reject
+    @all_words = to_match + to_reject
+    # @regex_string = regex_string
+    @regex = Regexp.new(regex_string)
+  end
 
-	def match_words?
-		@all_words.map do |word|
-			word.match(@regex).string unless word.match(@regex).nil?
-		end.compact == @to_match
-	end
+  def match_words?
+    @all_words.map do |word|
+      word.match(@regex).string unless word.match(@regex).nil?
+    end.compact == @to_match
+  end
 
 end
 
 class RegexPrinter
 
-	def initialize(regex_string)
-		@regex = Regexp.new(regex_string)
-	end
+  def initialize(regex_string)
+    @regex = Regexp.new(regex_string)
+  end
 
-	def regex_printer
-		"regex printed"
-	end
+  def regex_printer
+    "regex printed"
+  end
 
-	def remove_regex_delimiters # Regex Golf site requires delimiter removal
-		@regex.inspect[1...-1]
-	end
+  def remove_regex_delimiters # Regex Golf site requires delimiter removal
+    @regex.inspect[1...-1]
+  end
 
 end
 
--- regex-golf/test_regex_golf.rb
+++ regex-golf/test_regex_golf.rb
@@ -9,44 +9,46 @@ Minitest::Reporters.use!
 
 class TestRegexGolf < Minitest::Test
 
-	def setup
-		@to_match = ["abac", "accede", "adead", "babe", "bead",
-								"bebed", "bedad", "bedded", "bedead", "bedeaf",
-								"caba", "caffa", "dace", "dade", "daff", "dead", 
-								"deed", "deface", "faded", "faff", "feed"]
-		@to_reject = ["beam", "buoy", "canjac", "chymia", "corah", 
-								"cupula", "griece", "hafter", "idic", "lucy",
-								"martyr", "matron", "messrs", "mucose", "relose", 
-								"sonly", "tegua", "threap", "towned", "widish", "yite"]
-		
-		@all_words = 						@to_reject + @to_match
-	
-		
-		#@to_match_with_typo = 	(@ to_match[0..-2] << "veed").join(' ')
+  def setup
+    @to_match = ["abac", "accede", "adead", "babe", "bead",
+                "bebed", "bedad", "bedded", "bedead", "bedeaf",
+                "caba", "caffa", "dace", "dade", "daff", "dead", 
+                "deed", "deface", "faded", "faff", "feed"]
+    @to_reject = ["beam", "buoy", "canjac", "chymia", "corah", 
+                "cupula", "griece", "hafter", "idic", "lucy",
+                "martyr", "matron", "messrs", "mucose", "relose", 
+                "sonly", "tegua", "threap", "towned", "widish", "yite"]
+    
+    @all_words =            @to_reject + @to_match
+  
+    
+    #@to_match_with_typo =  (@ to_match[0..-2] << "veed").join(' ')
 
-		@regex_string = "[a-df][a-e][a-f]{2}\w?"
-		@r = RegexPrinter.new(@regex_string)
-		@m = 						StringMatcher.new(@to_match, @to_reject, @regex_string)
-		#@m_with_typo = StringMatcher.new(@to_match_with_typo, @regex_string)
-	end
+    @regex_string = "[a-df][a-e][a-f]{2}\w?"
+    @bad_regex_string = "/\w+/"
+    @r = RegexPrinter.new(@regex_string)
+    @m =            StringMatcher.new(@to_match, @to_reject, @regex_string)
+    #@m_with_typo = StringMatcher.new(@to_match_with_typo, @regex_string)
+  end
 
-	def test_regex_printer
-		assert_equal "regex printed", @r.regex_printer
-	end
+  def test_regex_printer
+    assert_equal "regex printed", @r.regex_printer
+  end
 
-	def test_match_words_method_passes_when_correct
-		assert @m.match_words?
-	end
+  def test_match_words_method_passes_when_correct
+    assert @m.match_words?
+  end
 
-	# def test_match_words_fails_when_match_word_list_has_typo
-	# 	refute @m_with_typo.match_words?
-	# end
+  def test_match_words_fails_with_bad_regex
+    @m_bad = StringMatcher.new(@to_match, @to_reject, @bad_regex_string)
+    refute @m_bad.match_words?
+  end
 
 
-	def test_final_result_is_string_no_delimiters
-		assert_equal String, @r.remove_regex_delimiters.class
-		assert_equal @regex_string, @r.remove_regex_delimiters
-	end
+  def test_final_result_is_string_no_delimiters
+    assert_equal String, @r.remove_regex_delimiters.class
+    assert_equal @regex_string, @r.remove_regex_delimiters
+  end
 
 
 end
@@ -54,44 +56,44 @@ end
 
 =begin
 
-	def test_words_to_match_populate_array
-		assert_equal to_match.class, Array
-		assert_equal to_match.count, to_match_input.split.count
-	end
+  def test_words_to_match_populate_array
+    assert_equal to_match.class, Array
+    assert_equal to_match.count, to_match_input.split.count
+  end
 
-	def test_words_to_reject_populate_array
-		assert_equal to_reject.class, Array
-		assert_equal to_reject.count, to_reject_input.split.count
-	end
+  def test_words_to_reject_populate_array
+    assert_equal to_reject.class, Array
+    assert_equal to_reject.count, to_reject_input.split.count
+  end
 
-	def test_prints_words_to_match_to_console
-	end
+  def test_prints_words_to_match_to_console
+  end
 
-	def test_prints_words_to_reject_to_console
-	end
+  def test_prints_words_to_reject_to_console
+  end
 
-	def test_accept_regex_with_forward_slash_delimiters
-	end
+  def test_accept_regex_with_forward_slash_delimiters
+  end
 
-	def test_accept_regex_with_no_delimiters
-	end
+  def test_accept_regex_with_no_delimiters
+  end
 
-	def test_print_success_if_matches_all
-	end
+  def test_print_success_if_matches_all
+  end
 
-	def test_print_success_if_matches_no_to_reject_words
-	end
+  def test_print_success_if_matches_no_to_reject_words
+  end
 
-	def test_print_fail_if_any_match_words_missing
-	end
+  def test_print_fail_if_any_match_words_missing
+  end
 
-	def test_print_fail_if_matches_any_to_reject_words
-	end
+  def test_print_fail_if_matches_any_to_reject_words
+  end
 
-	def test_regex_matches_zero_reject_words
-	end
+  def test_regex_matches_zero_reject_words
+  end
 
-	def test_regex_matches_all_accept_words
-	end
+  def test_regex_matches_all_accept_words
+  end
 
 =end
